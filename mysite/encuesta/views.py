c88a94800f6ca038dd9d0a5741be0859377681b4
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2022-08-25 21:42:51 -0500

    DAE-01

---
+++ mysite/encuesta/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Desde la visa de encuestas!")
+
+def detalle(request, pregunta_id):
+    return HttpResponse("Estas viendo la pregunta %s." % pregunta_id)
+
+def resultados(request, pregunta_id):
+    response = "Estas viendo los resultado de la pregunta %s."
+    return HttpResponse(response % pregunta_id)
+
+def votar(request, pregunta_id):
+    return HttpResponse("Estas votando por la pregunta %s." % pregunta_id)
+
+def sumar(request, a,b):
+    c=a+b
+    return HttpResponse("La suma de %s y %s es: %s" %(a,b,c))
